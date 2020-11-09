f67fcdfb60790ec1a7cf100424fc6ad88b921ca1
Anuj Rawat <rawatanuj835@gmail.com>
2020-11-09 19:10:23 +0530

    Commit for Authentication Api

---
+++ Authentication/Provider/ITokenProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Authentication.Models;
+
+namespace Authentication.Provider
+{
+	public interface ITokenProvider
+	{
+		public JwtSecurityToken GenerateJWTToken(Login model);
+	}
+}
